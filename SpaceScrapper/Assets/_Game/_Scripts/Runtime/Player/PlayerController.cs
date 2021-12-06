f55d2fd8c29c9a7e32c4747af1d0fd50e840ca06
Alpha Phi <alphasoldier1990@gmail.com>
2021-12-06 23:41:01 +0100

    Add Player Controller, Add Player Ship placeholder model, Add Player prefab, Add Sandbox scene

---
+++ SpaceScrapper/Assets/_Game/_Scripts/Runtime/Player/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceScrapper
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+            
+        }
+    }
+}
