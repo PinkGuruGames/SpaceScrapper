88ac7a6748af7086c7f445564d9cdc0fcb1fb70f
Frederick Ouimet <fouimet@gmail.com>
2021-12-07 15:28:13 -0500

    Add VoronoiLib

---
+++ SpaceScrapper/Assets/_Game/_Scripts/Runtime/MathLib/VoronoiLib/Structures/FortuneSite.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SpaceScrapper.MathLib.VoronoiLib.Structures
+{
+    public class FortuneSite: IEquatable<FortuneSite>
+    {
+        public static int IDCounter = 0;
+
+        public int ID { get; }
+        public double X { get; }
+        public double Y { get; }
+        public List<VPoint> Corners { get; }
+
+        public bool ToClose { get; set; } = false;
+
+        public HashSet<VEdge> Cell { get; private set; }
+
+        public HashSet<FortuneSite> Neighbors { get; private set; }
+
+        public FortuneSite(double x, double y)
+        {
+            ID = IDCounter++;
+            X = x;
+            Y = y;
+            Cell = new HashSet<VEdge>();
+            Neighbors = new HashSet<FortuneSite>();
+            Corners = new List<VPoint>();
+        }
+
+        public bool Equals(FortuneSite other)
+        {
+            return ID.Equals(other.ID);
+        }
+
+        public HashSet<VPoint> GetUniquePoints()
+        {
+            HashSet<VPoint> uniquePoints = new HashSet<VPoint>();
+            foreach (var v in Corners)
+            {
+                uniquePoints.Add(v);
+            }
+            foreach (var e in Cell)
+            {
+                uniquePoints.Add(e.Start);
+                uniquePoints.Add(e.End);
+            }
+
+            return uniquePoints;
+        }
+    }
+}
---
+++ SpaceScrapper/Assets/_Game/_Scripts/Runtime/MathLib/VoronoiLib/Structures/VPoint.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+
+namespace SpaceScrapper.MathLib.VoronoiLib.Structures
+{
+    public class VPoint: IEquatable<VPoint>
+    {
+        public double X { get; }
+        public double Y { get; }
+
+        internal VPoint(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Vector2 ToVector2()
+        {
+            return new Vector2((float)X, (float)Y);
+        }
+
+        public Vector3 ToVector3()
+        {
+            return new Vector3((float)X, (float)Y, 0f);
+        }
+
+        //public override bool Equals(object obj)
+        //{
+        //    if (obj != null)
+        //    {
+        //        return false;
+        //    }
+
+        //    VPoint other = (VPoint)obj;
+
+        //    return other.X.ApproxEqual(X) && other.Y.ApproxEqual(Y);
+        //}
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1861411795;
+            hashCode = hashCode * -1521134295 + X.GetHashCode();
+            hashCode = hashCode * -1521134295 + Y.GetHashCode();
+            return hashCode;
+        }
+
+        public bool Equals(VPoint other)
+        {
+            return other.X.ApproxEqual(X) && other.Y.ApproxEqual(Y);
+        }
+    }
+}
