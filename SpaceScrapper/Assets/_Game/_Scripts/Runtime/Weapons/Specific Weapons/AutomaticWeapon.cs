51728874f3fa1bdb63a13b0b36e373269a868fa0
Toreole <toreole.official@gmail.com>
2022-05-15 19:35:00 +0200

    Infinity shooting glitch fixed on automatic weapon
    Tracking an extra Time.time based variable to see if it should be possible to start shooting in the coroutine

--- SpaceScrapper/Assets/_Game/_Scripts/Runtime/Weapons/Specific Weapons/AutomaticWeapon.cs
+++ SpaceScrapper/Assets/_Game/_Scripts/Runtime/Weapons/Specific Weapons/AutomaticWeapon.cs
@@ -9,6 +9,11 @@ namespace SpaceScrapper.Weapons
     [SuppressMessage("ReSharper", "IteratorNeverReturns")]
     public class AutomaticWeapon : ReloadableWeapon
     {
+        /// <summary>
+        /// The time (Time.time) when the weapon has fired the last time.
+        /// </summary>
+        private float lastShotFired = 0f;
+
         protected internal override void ToggleShooting()
         {
             if (ShootAfterReload)
@@ -17,6 +22,7 @@ namespace SpaceScrapper.Weapons
                 return;
             }
             
+            //either turn on or turn off shooting.
             if(ShootingCoroutine is null)
                 ShootingCoroutine = StartCoroutine(Co_Shoot());
             else
@@ -26,12 +32,27 @@ namespace SpaceScrapper.Weapons
             }
         }
 
+        /// <summary>
+        /// Coroutine that handles automatic shooting.
+        /// </summary>
         private IEnumerator Co_Shoot()
         {
+            // Time between shots too short check: 
+            // next possible shot time - current time 
+            // negative difference = okay to shoot
+            // positive difference = wait 
+            float timeOffset = (lastShotFired + TimeBetweenShots) - Time.time;
+            if (timeOffset > 0)
+            {
+                yield return new WaitForSeconds(timeOffset);
+            }
+
+            //the standard shooting loop.
             var wait = new WaitForSeconds(TimeBetweenShots);
             while (true)
             {
                 Shoot();
+                lastShotFired = Time.time;
                 yield return wait;
             }
         }
