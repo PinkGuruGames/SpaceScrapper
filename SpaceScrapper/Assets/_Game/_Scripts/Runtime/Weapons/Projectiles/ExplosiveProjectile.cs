4b528dd9856ca8ee981b81f39b2c15ccfada2160
Toreole <toreole.official@gmail.com>
2022-05-29 17:14:23 +0200

    Preparing Demo Level

---
+++ SpaceScrapper/Assets/_Game/_Scripts/Runtime/Weapons/Projectiles/ExplosiveProjectile.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace SpaceScrapper.Weapons
+{
+    public class ExplosiveProjectile : ProjectileBase
+    {
+        protected override void Move()
+        {
+            //throw new System.NotImplementedException();
+        }
+
+        protected override void ProcessDamageEvent(IDamageable target, Collider2D hitCollider)
+        {
+            Explode();
+        }
+
+        protected override void ProcessProjectileHit(ProjectileBase other)
+        {
+            //Explosive Projectiles can be made to detonate by enemy linear projectiles.
+            if(SourceEntity.CanDamage(other.SourceEntity) && other is LinearProjectile)
+                Explode();
+        }
+
+        protected override void ProcessStaticHit(Collider2D other)
+        {
+            Explode();
+        }
+
+        protected void Explode()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
