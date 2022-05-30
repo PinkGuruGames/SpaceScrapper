7e981a7fc218737dc92cd1e35c203eb4b66abd3c
Toreole <toreole.official@gmail.com>
2022-05-30 17:03:59 +0200

    Added 3D models for Doors and Turret Enemies, Player Prefab with Weapon

---
+++ SpaceScrapper/Assets/_Game/_Scripts/Runtime/Environment/Door.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceScrapper
+{
+    /// <summary>
+    /// A very basic script for a door that can only be opened.
+    /// </summary>
+    [RequireComponent(typeof(Animator))]
+    public class Door : MonoBehaviour
+    {
+        private Animator animator;
+
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        /// <summary>
+        /// Set the animator trigger to cause the door to open.
+        /// </summary>
+        public void Open()
+        {
+            animator.SetTrigger("Open");
+        }
+    }
+}
